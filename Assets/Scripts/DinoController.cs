29b0f4cff64002cf75eec0e62ad555a9102939d2
Avea <aveakrause@gmail.com>
2020-06-20 11:32:29 -0700

    Added Dinoship
    wasd/arrows to move

---
+++ Assets/Scripts/DinoController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinoController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float speed = 5f;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        if(rb == null)
+            rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        if(moveInput != Vector2.zero)
+            Move();
+    }
+
+    private void Move()
+    {
+        rb.velocity = moveInput * speed;
+    }
+}
