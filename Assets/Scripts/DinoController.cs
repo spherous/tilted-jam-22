687a9d89c5996fd390cfc76f418d8f440b815bc1
Avea <aveakrause@gmail.com>
2020-06-20 12:45:49 -0700

    added lasers

--- Assets/Scripts/DinoController.cs
+++ Assets/Scripts/DinoController.cs
@@ -1,22 +1,57 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DinoController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Collider2D collider;
+    [SerializeField] private Transform firePointLeft;
+    [SerializeField] private Transform firePointRight;
+    [SerializeField] private GameObject laserPrefab;
     [SerializeField] private float speed = 5f;
+    [SerializeField] private float rotateSpeed = 5f;
+    [SerializeField] private float timeBetweenAttacks = 0.2f;
+    private float nextAttackTime = 0;
     private Vector2 moveInput;
+    private float rotateInput;
+    private bool shootInput;
+    private bool firePointToggle = false;
 
     void Start()
     {
         if(rb == null)
             rb = GetComponent<Rigidbody2D>();
+        if(collider == null)
+            collider = GetComponent<Collider2D>();
     }
 
     void Update()
     {
-        moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveInput = transform.up * Input.GetAxis("Vertical");
+        rotateInput = -Input.GetAxis("Horizontal");
+        shootInput = Input.GetKey(KeyCode.Space);
+
+        if(shootInput)
+            Shoot();
+    }
+
+    private void Shoot()
+    {
+        if(Time.timeSinceLevelLoad >= nextAttackTime)
+        {
+            GameObject newGO = firePointToggle
+                ? Instantiate(laserPrefab, firePointLeft.position, firePointLeft.rotation)
+                : Instantiate(laserPrefab, firePointRight.position, firePointRight.rotation);
+            
+            Physics2D.IgnoreCollision(collider, newGO.GetComponent<Collider2D>());
+
+            newGO?.GetComponent<Laser>()?.Fire();
+
+            firePointToggle = !firePointToggle;
+            nextAttackTime = Time.timeSinceLevelLoad + timeBetweenAttacks;
+        }
     }
 
     private void FixedUpdate()
@@ -27,5 +62,6 @@ public class DinoController : MonoBehaviour
     private void Move()
     {
         rb.velocity = moveInput * speed;
+        rb.angularVelocity = rotateInput * rotateSpeed;
     }
 }
---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float speed = 20f;
+    [SerializeField] private float lifeTime = 2f;
+    private float timeToDestroy;
+    
+    private bool fire = false;
+
+    private void Start()
+    {
+        if(rb == null)
+            rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(Time.timeSinceLevelLoad >= timeToDestroy)
+            Destroy(gameObject);
+    }
+
+    private void LateUpdate() {
+        if(fire)
+        {
+            rb.velocity = transform.up * speed;
+            fire = false;
+            timeToDestroy = Time.timeSinceLevelLoad + lifeTime;
+        }
+    }
+
+    public void Fire() => fire = true;
+}
