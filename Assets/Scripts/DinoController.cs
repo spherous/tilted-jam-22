7f8300fc89c71e53f88e5924d29ba84e93198b32
Avea <aveakrause@gmail.com>
2020-06-20 18:01:50 -0700

    added game over screen
    added some audio

--- Assets/Scripts/DinoController.cs
+++ Assets/Scripts/DinoController.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class DinoController : MonoBehaviour, IDamagable, IHealth
 {
@@ -25,7 +26,11 @@ public class DinoController : MonoBehaviour, IDamagable, IHealth
     private int currentHealth;
     public int MaxHealth {get {return maxHealth;} set{}}
     public int CurrentHealth {get{return currentHealth;} set{}}
+    public delegate void OnHealthChanged(int newHealth);
+    public OnHealthChanged onHealthChanged;
 
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip deathSound;
 
     void Start()
     {
@@ -45,12 +50,15 @@ public class DinoController : MonoBehaviour, IDamagable, IHealth
         if(shootInput)
             Shoot();
 
-        if(Input.GetKeyDown(KeyCode.G))
-            FullHeal();
+        // if(Input.GetKeyDown(KeyCode.G))
+        //     FullHeal();
     }
 
     private void Shoot()
     {
+        if(EventSystem.current.IsPointerOverGameObject())
+            return;
+
         if(Time.timeSinceLevelLoad >= nextAttackTime)
         {
             GameObject newGO = firePointToggle
@@ -91,10 +99,22 @@ public class DinoController : MonoBehaviour, IDamagable, IHealth
     public void LoseHealth(int amount)
     {
         currentHealth = Mathf.Max(0, CurrentHealth - amount);
+        onHealthChanged.Invoke(CurrentHealth);
         if(CurrentHealth == 0)
+        {
+            audioSource.PlayOneShot(deathSound);
             gm.EndGame();
+        }
     }
 
-    public void GainHealth(int amount) => currentHealth = Mathf.Min(MaxHealth, CurrentHealth + amount);
-    public void FullHeal() => currentHealth = MaxHealth;
+    public void GainHealth(int amount)
+    {
+        currentHealth = Mathf.Min(MaxHealth, CurrentHealth + amount);
+        onHealthChanged.Invoke(CurrentHealth);
+    }
+    public void FullHeal()
+    {
+        currentHealth = MaxHealth;
+        onHealthChanged.Invoke(CurrentHealth);
+    }
 }
---
+++ Assets/Scripts/RetryButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RetryButton : MonoBehaviour
+{
+    [SerializeField] private Button button;
+    private void Awake() => button.onClick.AddListener(RestartScene);
+    private void RestartScene() => SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+}
