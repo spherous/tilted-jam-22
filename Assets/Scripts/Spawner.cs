558fa34dea3b4800bdc0cc97391e6d8051d08fe5
Jacob Christ <jacob@pontech.com>
2020-06-20 17:26:34 -0700

    Improved spawner script with greater index zero spawning.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,6 +6,7 @@ public class Spawner : MonoBehaviour
 {
     public GameObject[] SpawnObject;
     public float SpawnRateInSeconds = 5.0f;
+    public float IndexZeroSpanwChance = 0.5f;
     public float x_min = 0;
     public float x_max = 0;
     public float y_min = 0;
@@ -23,11 +24,17 @@ public class Spawner : MonoBehaviour
     {
         while (true)
         {
-            Instantiate(SpawnObject[Random.Range(0, SpawnObject.Length)], 
+            int index = 0;
+
+            if (Random.Range(0f,1f) > IndexZeroSpanwChance)
+                index = Random.Range(0, SpawnObject.Length);
+
+            Instantiate(SpawnObject[index],
                         new Vector3(Random.Range(x_min, x_max),
-                                    Random.Range(y_min, y_max), 
-                                    Random.Range(z_min, z_max)), 
+                                    Random.Range(y_min, y_max),
+                                    Random.Range(z_min, z_max)),
                         Quaternion.identity);
+
             yield return new WaitForSeconds(SpawnRateInSeconds);
         }
     }
